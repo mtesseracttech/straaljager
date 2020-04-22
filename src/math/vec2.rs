13b7403e1fbdae5b8db82d9a7a257846cd7ae9e9
Hector Peeters <hector.peeters@gmail.com>
2020-04-22 11:28:33 +0200

    Add tests for vec2, vec3 and vec4 and changed approx_eq

--- src/math/vec2.rs
+++ src/math/vec2.rs
@@ -2,7 +2,7 @@ use crate::math::{approx_eq, PhysicsVector};
 use std::fmt;
 use std::ops::{Add, Div, Index, IndexMut, Mul, Neg, Sub};
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
@@ -48,7 +48,7 @@ impl Vec2 {
     }
 
     pub fn is_unit(&self) -> bool {
-        approx_eq(&self.length_squared(), &1.0)
+        approx_eq(self.length_squared(), 1.0)
     }
 }
 
@@ -180,7 +180,7 @@ impl Mul<Vec2> for f32 {
 ///
 impl PartialEq for Vec2 {
     fn eq(&self, other: &Self) -> bool {
-        approx_eq(&self.x, &other.x) && approx_eq(&self.y, &other.y)
+        approx_eq(self.x, other.x) && approx_eq(self.y, other.y)
     }
 }
 
@@ -236,4 +236,132 @@ impl PhysicsVector for Vec2 {
             Some(eta * *i - (eta * n_dot_i + k.sqrt()) * *n)
         }
     }
+}
+#[test]
+fn test_vec2_new() {
+    let vec: Vec2 = Vec2::new(1.0, 2.0);
+    assert!(approx_eq(vec.x, 1.0));
+    assert!(approx_eq(vec.y, 2.0));
+}
+
+#[test]
+fn test_vec2_dot() {
+    let vec1: Vec2 = Vec2::new(1.0, 2.0);
+    let vec2: Vec2 = Vec2::new(5.0, 6.0);
+    assert!(approx_eq(vec1.dot(&vec2), 17.0));
+
+    let vec2: Vec2 = Vec2::new(1.0, 1.0);
+    assert!(approx_eq(vec2.dot(&vec2), 2.0))
+}
+
+#[test]
+fn test_vec2_length_squared() {
+    let vec1: Vec2 = Vec2::new(1.0, 3.0);
+    assert!(approx_eq(vec1.length_squared(), vec1.dot(&vec1)));
+}
+
+#[test]
+fn test_vec2_length() {
+    let vec1: Vec2 = Vec2::new(1.0, 0.0);
+    assert!(approx_eq(vec1.length(), 1.0));
+
+    let vec2: Vec2 = Vec2::new(1.0, -1.0);
+    assert!(approx_eq(vec2.length(), 2.0f32.sqrt()));
+
+    let vec2: Vec2 = Vec2::new(2.0, 4.0);
+    assert!(approx_eq(vec2.length(), 20.0f32.sqrt()));
+}
+
+#[test]
+fn test_vec2_normalized() {
+    let vec1: Vec2 = Vec2::new(4.0, -6.0);
+    let vec1_norm: Vec2 = vec1.normalized();
+
+    assert!(vec1_norm.is_unit());
+
+    let diff = vec1 / vec1_norm;
+    assert!(approx_eq(diff.x, diff.y));
+}
+
+#[test]
+fn test_vec2_normalize() {
+    let mut vec1: Vec2 = Vec2::new(4.0, -6.0);
+    vec1.normalize();
+
+    assert!(vec1.is_unit());
+}
+
+#[test]
+fn test_vec2_negate() {
+    let vec: Vec2 = -Vec2::new(1.0, -2.0);
+
+    assert_eq!(vec, Vec2::new(-1.0, 2.0));
+}
+
+#[test]
+fn test_vec2_add_vec2() {
+    let vec1: Vec2 = Vec2::new(1.0, -2.0);
+    let vec2: Vec2 = Vec2::new(-5.0, 6.0);
+    let result: Vec2 = vec1 + vec2;
+
+    assert_eq!(result, Vec2::new(-4.0, 4.0));
+}
+
+#[test]
+fn test_vec2_sub_vec2() {
+    let vec1: Vec2 = Vec2::new(1.0, -2.0);
+    let vec2: Vec2 = Vec2::new(-5.0, 6.0);
+    let result: Vec2 = vec1 - vec2;
+
+    assert_eq!(result, Vec2::new(6.0, -8.0));
+}
+
+#[test]
+fn test_vec2_mul_vec2() {
+    let vec1: Vec2 = Vec2::new(1.0, -2.0);
+    let vec2: Vec2 = Vec2::new(-5.0, 6.0);
+    let result: Vec2 = vec1 * vec2;
+
+    assert_eq!(result, Vec2::new(-5.0, -12.0));
+}
+
+
+#[test]
+fn test_vec2_div_vec2() {
+    let vec1: Vec2 = Vec2::new(5.0, -6.0);
+    let vec2: Vec2 = Vec2::new(-2.0, 3.0);
+    let result: Vec2 = vec1 / vec2;
+
+    assert_eq!(result, Vec2::new(-2.5, -2.0));
+}
+
+#[test]
+fn test_vec2_mul_scalar() {
+    let vec1: Vec2 = Vec2::new(5.0, -6.0);
+    let result: Vec2 = vec1 * -2.0;
+
+    assert_eq!(result, Vec2::new(-10.0, 12.0));
+}
+
+#[test]
+fn test_vec2_div_scalar() {
+    let vec1: Vec2 = Vec2::new(5.0, -6.0);
+    let result: Vec2 = vec1 / -2.0;
+
+    assert_eq!(result, Vec2::new(-2.5, 3.0));
+}
+
+#[test]
+fn test_vec2_mul_scalar_reversed() {
+    let vec1: Vec2 = Vec2::new(5.0, -6.0);
+    let result: Vec2 = -2.0 * vec1;
+
+    assert_eq!(result, Vec2::new(-10.0, 12.0));
+}
+
+#[test]
+fn test_vec2_index() {
+    let vec1: Vec2 = Vec2::new(5.0, -6.0);
+    assert!(approx_eq(vec1[0], 5.0));
+    assert!(approx_eq(vec1[1], -6.0));
 }
--- src/math/vec4.rs
+++ src/math/vec4.rs
@@ -2,7 +2,7 @@ use crate::math::{approx_eq, PhysicsVector};
 use std::fmt;
 use std::ops::{Add, Div, Index, IndexMut, Mul, Neg, Sub};
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct Vec4 {
     pub x: f32,
     pub y: f32,
@@ -12,7 +12,7 @@ pub struct Vec4 {
 
 impl Vec4 {
     pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
-        Self { x, y, z, w}
+        Self { x, y, z, w }
     }
 
     pub fn zero() -> Self {
@@ -59,13 +59,17 @@ impl Vec4 {
     }
 
     pub fn is_unit(&self) -> bool {
-        approx_eq(&self.length_squared(), &1.0)
+        approx_eq(self.length_squared(), 1.0)
     }
 }
 
 impl fmt::Display for Vec4 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Vec4 {{x: {}, y: {}, z: {}, w: {}}}", self.x, self.y, self.z, self.w)
+        write!(
+            f,
+            "Vec4 {{x: {}, y: {}, z: {}, w: {}}}",
+            self.x, self.y, self.z, self.w
+        )
     }
 }
 
@@ -207,7 +211,10 @@ impl Mul<Vec4> for f32 {
 ///
 impl PartialEq for Vec4 {
     fn eq(&self, other: &Self) -> bool {
-        approx_eq(&self.x, &other.x) && approx_eq(&self.y, &other.y) && approx_eq(&self.z, &other.z) && approx_eq(&self.w, &other.w)
+        approx_eq(self.x, other.x)
+            && approx_eq(self.y, other.y)
+            && approx_eq(self.z, other.z)
+            && approx_eq(self.w, other.w)
     }
 }
 
@@ -269,4 +276,137 @@ impl PhysicsVector for Vec4 {
             Some(eta * *i - (eta * n_dot_i + k.sqrt()) * *n)
         }
     }
+}
+
+#[test]
+fn test_vec4_new() {
+    let vec: Vec4 = Vec4::new(1.0, 2.0, 3.0, 4.0);
+    assert!(approx_eq(vec.x, 1.0));
+    assert!(approx_eq(vec.y, 2.0));
+    assert!(approx_eq(vec.z, 3.0));
+    assert!(approx_eq(vec.w, 4.0));
+}
+
+#[test]
+fn test_vec4_dot() {
+    let vec1: Vec4 = Vec4::new(1.0, 2.0, 3.0, 4.0);
+    let vec2: Vec4 = Vec4::new(5.0, 6.0, 7.0, 8.0);
+    assert!(approx_eq(vec1.dot(&vec2), 70.0));
+
+    let vec3: Vec4 = Vec4::new(1.0, 1.0, 1.0, 1.0);
+    assert!(approx_eq(vec3.dot(&vec3), 4.0))
+}
+
+#[test]
+fn test_vec4_length_squared() {
+    let vec1: Vec4 = Vec4::new(1.0, 3.0, 4.0, -4.0);
+    assert!(approx_eq(vec1.length_squared(), vec1.dot(&vec1)));
+}
+
+#[test]
+fn test_vec4_length() {
+    let vec1: Vec4 = Vec4::new(1.0, 0.0, 0.0, 0.0);
+    assert!(approx_eq(vec1.length(), 1.0));
+
+    let vec2: Vec4 = Vec4::new(1.0, -1.0, 1.0, -1.0);
+    assert!(approx_eq(vec2.length(), 2.0));
+
+    let vec2: Vec4 = Vec4::new(2.0, 4.0, 5.0, -6.0);
+    assert!(approx_eq(vec2.length(), 9.0));
+}
+
+#[test]
+fn test_vec4_normalized() {
+    let vec1: Vec4 = Vec4::new(4.0, -6.0, 8.0, -12.0);
+    let vec1_norm: Vec4 = vec1.normalized();
+
+    assert!(vec1_norm.is_unit());
+
+    let diff = vec1 / vec1_norm;
+    assert!(approx_eq(diff.x, diff.y) && approx_eq(diff.y, diff.z) && approx_eq(diff.z, diff.w));
+}
+
+#[test]
+fn test_vec4_normalize() {
+    let mut vec1: Vec4 = Vec4::new(4.0, -6.0, 8.0, -12.0);
+    vec1.normalize();
+
+    assert!(vec1.is_unit());
+}
+
+#[test]
+fn test_vec4_negate() {
+    let vec: Vec4 = -Vec4::new(1.0, -2.0, 3.0, -4.0);
+
+    assert_eq!(vec, Vec4::new(-1.0, 2.0, -3.0, 4.0));
+}
+
+#[test]
+fn test_vec4_add_vec4() {
+    let vec1: Vec4 = Vec4::new(1.0, -2.0, 3.0, -4.0);
+    let vec2: Vec4 = Vec4::new(-5.0, 6.0, -7.0, 8.0);
+    let result: Vec4 = vec1 + vec2;
+
+    assert_eq!(result, Vec4::new(-4.0, 4.0, -4.0, 4.0));
+}
+
+#[test]
+fn test_vec4_sub_vec4() {
+    let vec1: Vec4 = Vec4::new(1.0, -2.0, 3.0, -4.0);
+    let vec2: Vec4 = Vec4::new(-5.0, 6.0, -7.0, 8.0);
+    let result: Vec4 = vec1 - vec2;
+
+    assert_eq!(result, Vec4::new(6.0, -8.0, 10.0, -12.0));
+}
+
+#[test]
+fn test_vec4_mul_vec4() {
+    let vec1: Vec4 = Vec4::new(1.0, -2.0, 3.0, -4.0);
+    let vec2: Vec4 = Vec4::new(-5.0, 6.0, -7.0, 8.0);
+    let result: Vec4 = vec1 * vec2;
+
+    assert_eq!(result, Vec4::new(-5.0, -12.0, -21.0, -32.0));
+}
+
+
+#[test]
+fn test_vec4_div_vec4() {
+    let vec1: Vec4 = Vec4::new(5.0, -6.0, 7.0, -8.0);
+    let vec2: Vec4 = Vec4::new(-2.0, 3.0, -2.0, 2.0);
+    let result: Vec4 = vec1 / vec2;
+
+    assert_eq!(result, Vec4::new(-2.5, -2.0, -3.5, -4.0));
+}
+
+#[test]
+fn test_vec4_mul_scalar() {
+    let vec1: Vec4 = Vec4::new(5.0, -6.0, 7.0, -8.0);
+    let result: Vec4 = vec1 * -2.0;
+
+    assert_eq!(result, Vec4::new(-10.0, 12.0, -14.0, 16.0));
+}
+
+#[test]
+fn test_vec4_div_scalar() {
+    let vec1: Vec4 = Vec4::new(5.0, -6.0, 7.0, -8.0);
+    let result: Vec4 = vec1 / -2.0;
+
+    assert_eq!(result, Vec4::new(-2.5, 3.0, -3.5, 4.0));
+}
+
+#[test]
+fn test_vec4_mul_scalar_reversed() {
+    let vec1: Vec4 = Vec4::new(5.0, -6.0, 7.0, -8.0);
+    let result: Vec4 = -2.0 * vec1;
+
+    assert_eq!(result, Vec4::new(-10.0, 12.0, -14.0, 16.0));
+}
+
+#[test]
+fn test_vec4_index() {
+    let vec1: Vec4 = Vec4::new(5.0, -6.0, 7.0, -8.0);
+    assert!(approx_eq(vec1[0], 5.0));
+    assert!(approx_eq(vec1[1], -6.0));
+    assert!(approx_eq(vec1[2], 7.0));
+    assert!(approx_eq(vec1[3], -8.0));
 }
