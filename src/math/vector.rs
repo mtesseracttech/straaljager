eb7205091f5b23fb7f5faaba972036469546cf32
Hector Peeters <hector.peeters@gmail.com>
2021-04-01 22:19:01 +0200

    Add all vector scalar binary operations

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -20,16 +20,26 @@ pub struct Vector<T, const S: usize> {
 }
 
 impl<T, const S: usize> Vector<T, S> {
+    ///
+    /// Create a vector from a slice with type T and size S
+    ///
     pub fn from_slice(e: [T; S]) -> Self {
         Self { e }
     }
 
-    pub fn size(&self) -> usize {
+    ///
+    /// The amount of elements this vector contains
+    ///
+    pub const fn size(&self) -> usize {
         S
     }
 }
 
 impl<T: Default + Copy + Mul<Output = T> + AddAssign, const S: usize> Vector<T, S> {
+    ///
+    /// Dot product of two vectors by pairwise multiplication and taking the sum
+    /// of the resulting vector
+    ///
     pub fn dot(&self, other: &Self) -> T {
         let mut result = T::default();
         for i in 0..S {
@@ -38,6 +48,10 @@ impl<T: Default + Copy + Mul<Output = T> + AddAssign, const S: usize> Vector<T, 
         result
     }
 
+    ///
+    /// The squared length of a vector using pairwise multiplicaion and taking
+    /// the sum of the resulting vector
+    ///
     pub fn length_squared(&self) -> T {
         let mut result = T::default();
         for i in 0..S {
@@ -48,6 +62,9 @@ impl<T: Default + Copy + Mul<Output = T> + AddAssign, const S: usize> Vector<T, 
 }
 
 impl<const S: usize> Vector<f32, S> {
+    ///
+    /// The length of the vector
+    ///
     pub fn length(&self) -> f32 {
         let mut result = 0.0;
         for i in 0..S {
@@ -56,6 +73,9 @@ impl<const S: usize> Vector<f32, S> {
         result.sqrt()
     }
 
+    ///
+    /// Get a normalized version of this vector
+    ///
     pub fn normalized(&self) -> Self {
         let one_over_length = 1.0 / self.length();
         let mut e = [0.0; S];
@@ -65,6 +85,9 @@ impl<const S: usize> Vector<f32, S> {
         Vector::<f32, S> { e }
     }
 
+    ///
+    /// Normalize the vector in-place
+    ///
     pub fn normalize(&mut self) {
         let one_over_length = 1.0 / self.length();
         for i in 0..S {
@@ -72,6 +95,9 @@ impl<const S: usize> Vector<f32, S> {
         }
     }
 
+    ///
+    /// Check if this vector is a unit vector
+    ///
     pub fn is_unit(&self) -> bool {
         approx_eq(self.length(), 1.0)
     }
@@ -79,6 +105,9 @@ impl<const S: usize> Vector<f32, S> {
 
 macro_rules! impl_binop {
     ($Op: ident, $op_fn: ident, $sym: tt) => {
+        ///
+        /// Vector vector math operation
+        ///
         impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<Vector<T, S>>
             for Vector<T, S>
         {
@@ -93,6 +122,9 @@ macro_rules! impl_binop {
             }
         }
 
+        ///
+        /// Vector vector reference math operation
+        ///
         impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<&Vector<T, S>>
             for Vector<T, S>
         {
@@ -107,6 +139,9 @@ macro_rules! impl_binop {
             }
         }
 
+        ///
+        /// Vector reference vector math operation
+        ///
         impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<Vector<T, S>>
             for &Vector<T, S>
         {
@@ -121,6 +156,9 @@ macro_rules! impl_binop {
             }
         }
 
+        ///
+        /// Vector reference vector reference math operation
+        ///
         impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<&Vector<T, S>>
             for &Vector<T, S>
         {
@@ -130,6 +168,62 @@ macro_rules! impl_binop {
                 let mut e = [T::default(); S];
                 for i in 0..S {
                     e[i] = self.e[i] $sym other.e[i];
+                }
+                Vector::<T, S> { e }
+            }
+        }
+
+        ///
+        /// Vector scalar math operation
+        ///
+        impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<T> for Vector<T, S> {
+            type Output = Self;
+            fn $op_fn(self, other: T) -> Self::Output {
+                let mut e = [T::default(); S];
+                for i in 0..S {
+                    e[i] = self.e[i] $sym other;
+                }
+                Vector::<T, S> { e }
+            }
+        }
+
+        ///
+        /// Vector scalar reference math operation
+        ///
+        impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<&T> for Vector<T, S> {
+            type Output = Self;
+            fn $op_fn(self, other: &T) -> Self::Output {
+                let mut e = [T::default(); S];
+                for i in 0..S {
+                    e[i] = self.e[i] $sym *other;
+                }
+                Vector::<T, S> { e }
+            }
+        }
+
+        ///
+        /// Vector reference scalar math operation
+        ///
+        impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<T> for &Vector<T, S> {
+            type Output = Vector<T, S>;
+            fn $op_fn(self, other: T) -> Self::Output {
+                let mut e = [T::default(); S];
+                for i in 0..S {
+                    e[i] = self.e[i] $sym other;
+                }
+                Vector::<T, S> { e }
+            }
+        }
+
+        ///
+        /// Vector reference scalar reference math operation
+        ///
+        impl<T: Default + Copy + $Op<Output = T>, const S: usize> $Op<&T> for &Vector<T, S> {
+            type Output = Vector<T, S>;
+            fn $op_fn(self, other: &T) -> Self::Output {
+                let mut e = [T::default(); S];
+                for i in 0..S {
+                    e[i] = self.e[i] $sym *other;
                 }
                 Vector::<T, S> { e }
             }
@@ -141,30 +235,6 @@ impl_binop!(Add, add, +);
 impl_binop!(Sub, sub, -);
 impl_binop!(Mul, mul, *);
 impl_binop!(Div, div, /);
-
-impl<T: Default + Copy + Div<Output = T>, const S: usize> Div<T> for Vector<T, S> {
-    type Output = Self;
-
-    fn div(self, other: T) -> Self::Output {
-        let mut e = [T::default(); S];
-        for i in 0..S {
-            e[i] = self.e[i] / other;
-        }
-        Vector::<T, S> { e }
-    }
-}
-
-impl<T: Default + Copy + Mul<Output = T>, const S: usize> Mul<T> for Vector<T, S> {
-    type Output = Self;
-
-    fn mul(self, other: T) -> Self::Output {
-        let mut e = [T::default(); S];
-        for i in 0..S {
-            e[i] = self.e[i] * other;
-        }
-        Vector::<T, S> { e }
-    }
-}
 
 ///
 /// Negate operator:
@@ -198,6 +268,9 @@ impl<T: Neg<Output = T> + Default + Copy, const S: usize> Neg for &Vector<T, S> 
     }
 }
 
+///
+/// Index accessor
+///
 impl<T, const S: usize> Index<usize> for Vector<T, S> {
     type Output = T;
 
@@ -207,6 +280,9 @@ impl<T, const S: usize> Index<usize> for Vector<T, S> {
     }
 }
 
+///
+/// Mutable index accessor
+///
 impl<T, const S: usize> IndexMut<usize> for Vector<T, S> {
     fn index_mut(&mut self, i: usize) -> &mut Self::Output {
         debug_assert!(i < S);
@@ -214,6 +290,9 @@ impl<T, const S: usize> IndexMut<usize> for Vector<T, S> {
     }
 }
 
+///
+/// PartialEq implementation using approx_eq
+///
 impl<T: ApproxEq, const S: usize> PartialEq for Vector<T, S> {
     fn eq(&self, other: &Self) -> bool {
         self.e
@@ -223,6 +302,9 @@ impl<T: ApproxEq, const S: usize> PartialEq for Vector<T, S> {
     }
 }
 
+///
+/// Debug implementation which prints elements and size
+///
 impl<T: Debug, const S: usize> Debug for Vector<T, S> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Vector")
@@ -234,6 +316,22 @@ impl<T: Debug, const S: usize> Debug for Vector<T, S> {
 
 // Size specific implemntations
 
+impl<T: Mul<Output = T> + Sub<Output = T> + Neg<Output = T> + Copy> Vector<T, 3> {
+    ///
+    /// Cross product implementation which is only implemented for vectors of
+    /// size 3
+    ///
+    pub fn cross(&self, other: &Self) -> Self {
+        Self {
+            e: [
+                self.e[1] * other.e[2] - self.e[2] * other.e[1],
+                -(self.e[0] * other.e[2] - self.e[2] * other.e[0]),
+                self.e[0] * other.e[1] - self.e[1] * other.e[0],
+            ],
+        }
+    }
+}
+
 impl<T> Vector<T, 2> {
     pub fn new(x: T, y: T) -> Self {
         Self { e: [x, y] }
@@ -243,18 +341,6 @@ impl<T> Vector<T, 2> {
 impl<T> Vector<T, 3> {
     pub fn new(x: T, y: T, z: T) -> Self {
         Self { e: [x, y, z] }
-    }
-}
-
-impl<T: Mul<Output = T> + Sub<Output = T> + Neg<Output = T> + Copy> Vector<T, 3> {
-    pub fn cross(&self, other: &Self) -> Self {
-        Self {
-            e: [
-                self.e[1] * other.e[2] - self.e[2] * other.e[1],
-                -(self.e[0] * other.e[2] - self.e[2] * other.e[0]),
-                self.e[0] * other.e[1] - self.e[1] * other.e[0],
-            ],
-        }
     }
 }
 
